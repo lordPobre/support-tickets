0fc6150b887f277011ce54ae8145bfd955a3ae2b
Carlos Lopez <clopez.ti@vcchile.cl>
2026-06-01 14:14:53 -0400

    correcion toml

--- support_system/urls.py
+++ support_system/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("health/", lambda request: HttpResponse("ok"), name="health"),
     path("", include("tickets.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
